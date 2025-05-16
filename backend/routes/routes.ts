24e32a38c684ec9033fefe0c174ab8ca9d8d9c89
Genrey <109770981+Genrei123@users.noreply.github.com>
2025-05-17 07:00:47 +0800

    Deleted two other routes

--- backend/routes/routes.ts
+++ backend/routes/routes.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import { TGPController, SouthstarController, allController } from "../controllers/scrape";
+import { allController } from "../controllers/scrape";
 
 const scrapeRoute = Router();
 
-scrapeRoute.get("/tgp", TGPController);
-scrapeRoute.get("/southstar", SouthstarController);
 scrapeRoute.get("/all", allController);
 
 export default scrapeRoute;
